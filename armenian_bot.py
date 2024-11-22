8092ede786cfe2277549d738073f4b5e75169520
Levon Ter-Grigoryan <leonidusspartus@gmail.com>
2024-11-22 23:12:44 +0100

    Add base scheduling logic

--- armenian_bot.py
+++ armenian_bot.py
@@ -1,10 +1,14 @@
+import asyncio
+import datetime
 import enum
 import configparser
 import logging
 import random
+import threading
+import schedule
 
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
 ### Initialize logging subsystem
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -28,38 +32,40 @@ alphabet = [
     ArmenianChar('Բբ', 'Б'),
     ArmenianChar('Գգ', 'Г'),
     ArmenianChar('Դդ', 'Д'),
-    ArmenianChar('Եե', 'Э'),
+    ArmenianChar('Եե', 'Е'),
     ArmenianChar('Զզ', 'З'),
+    ArmenianChar('Էէ','Э'),
+    ArmenianChar('Ըը','Ы'),
+    ArmenianChar('Թթ','Т'),
+    ArmenianChar('Ժժ','Ж'),
     ArmenianChar('Իի', 'И'),
-    ArmenianChar('Եե', 'Э'),
     ArmenianChar('Լլ', 'Л'),
     ArmenianChar('Խխ', 'Х'),
     ArmenianChar('Ծծ', 'Ц'),
     ArmenianChar('Կկ', 'К'),
     ArmenianChar('Հհ', 'Х'),
-    ArmenianChar('Ձձ', 'Д'),
-    ArmenianChar('Ղղ', 'Г'),
+    ArmenianChar('Ձձ', 'Дз'),
+    ArmenianChar('Ղղ', 'Р'),
     ArmenianChar('Ճճ', 'Ч'),
     ArmenianChar('Մմ', 'М'),
     ArmenianChar('Յյ', 'Й'),
     ArmenianChar('Նն', 'Н'),
     ArmenianChar('Շշ', 'Ш'),
-    ArmenianChar('Ոո', 'О'),
+    ArmenianChar('Ոո', 'Во'),
     ArmenianChar('Չչ', 'Ч'),
     ArmenianChar('Պպ', 'П'),
-    ArmenianChar('Ջջ', 'Ж'),
+    ArmenianChar('Ջջ', 'Дж'),
     ArmenianChar('Ռռ', 'Р'),
     ArmenianChar('Սս', 'С'),
     ArmenianChar('Վվ', 'В'),
     ArmenianChar('Տտ', 'Т'),
     ArmenianChar('Րր', 'Р'),
     ArmenianChar('Ցց', 'Ц'),
-    ArmenianChar('Ււ', 'У'),
-    ArmenianChar('Փփ', 'Ф'),
+    ArmenianChar('Ււ/Ուու', 'В'),
+    ArmenianChar('Փփ', 'П'),
     ArmenianChar('Քք', 'К'),
     ArmenianChar('Օօ', 'О'),
     ArmenianChar('Ֆֆ', 'Ф'),
-    ArmenianChar('և', 'Ев')  # Союз "и"
 ]
 
 class UserConversationState(enum.IntEnum):
@@ -67,10 +73,13 @@ class UserConversationState(enum.IntEnum):
     TRAIN_LETTER = 1
 
 class UserInfo:
-    def __init__(self):
+    def __init__(self, chat_id):
         self.conversation_state = UserConversationState.NONE
         self.letter_id = -1
         self.asked_letter_id = 0
+        self.chat_id = chat_id
+        self.is_subscribed = False
+        self.last_conversation_trigger = datetime.datetime.now()
 
     def asked_letter(self) -> ArmenianChar:
         return alphabet[self.asked_letter_id]
@@ -97,18 +106,33 @@ async def help(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
 def get_user_info(update: Update) -> UserInfo:
     chat_id = update.message.chat.id
     if chat_id not in user_id_to_info:
-        user_id_to_info[chat_id] = UserInfo()
+        user_id_to_info[chat_id] = UserInfo(chat_id)
 
     return user_id_to_info[chat_id]
 
 async def reset_my_state(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    get_user_info(update).conversation_state = UserConversationState.NONE
-    await update.message.reply_text(f'Понял. Давай начнем все заного!')
+    reply = f'Понял. Давай начнем все заного!'
+    user_info = get_user_info(update)
+    if user_info.conversation_state == UserConversationState.TRAIN_LETTER:
+        reply += f'\nЭто кстати была буква {user_info.asked_letter().char}'
+    user_info.conversation_state = UserConversationState.NONE
+    await update.message.reply_text(reply)
 
 async def learn_alphabet(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     letter: ArmenianChar = get_user_info(update).next_letter()
     await update.message.reply_text(f'{letter.char}. {letter.description()}')
 
+async def subscribe_to_daily_train(
+        update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_info = get_user_info(update)
+    user_info.is_subscribed = True
+    user_info.last_conversation_trigger = datetime.datetime.now()
+    await update.message.reply_text('Поздравля! Теперь я буду присылать тебе задания раз в день!')
+
+async def unsubscribe_from_daily_train(
+        update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    get_user_info(update).is_subscribed = False
+    await update.message.reply_text('Очень жаль что мы больше не друзья =(')
 
 async def conversation(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_info = get_user_info(update)
@@ -126,12 +150,37 @@ async def conversation(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
     await update.message.reply_text(reply_message)
 
+
+async def scheduled_conversation(app: Application):
+    logging.info('Checking subscriptions')
+    for (_, user_info) in user_id_to_info.items():
+        now = datetime.datetime.now()
+        total_diff = (now - user_info.last_conversation_trigger).total_seconds()
+        logging.info(f'Checking user which is {user_info.is_subscribed}. Seconds since the last udate {total_diff}')
+        if (user_info.is_subscribed and total_diff > 10):
+            user_info.last_conversation_trigger = now
+            await app.bot.send_message(chat_id=user_info.chat_id, text='Бу! Испугался? Не бойся, я друг, я тебя не обижу. Иди сюда, иди ко мне, сядь рядом со мной, посмотри мне в глаза. Ты видишь меня?')
+
+def schedule_sync(app: Application):
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(scheduled_conversation(app))
+
+def schedule_loop(app):
+    schedule.every(10).seconds.do(lambda: schedule_sync(app))
+    while True:
+        schedule.run_pending()
+
 def main():
     """Run bot."""
     app = ApplicationBuilder().token(token).build()
+
+    schedule_thread = threading.Thread(target=schedule_loop, args=[app])
+    schedule_thread.start()
     app.add_handler(CommandHandler("help", help))
     app.add_handler(CommandHandler("learn_alphabet", learn_alphabet))
     app.add_handler(CommandHandler("reset_my_state", reset_my_state))
+    app.add_handler(CommandHandler("subscribe_to_daily_train", subscribe_to_daily_train))
+    app.add_handler(CommandHandler("unsubscribe_from_daily_train", unsubscribe_from_daily_train))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, conversation))
     app.run_polling()
 
